e446ec05670266ece114369474506888a186d581
준우 이 <gs04043@naver.com>
2024-03-05 22:03:40 +0900

    1.1 xml 계층

--- src/test/java/study/tobi/IocTest.java
+++ src/test/java/study/tobi/IocTest.java
@@ -4,13 +4,16 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import study.tobi.ioc.bean.Hello;
+import study.tobi.ioc.bean.Printer;
 
 @ExtendWith(SpringExtension.class)
-@ContextConfiguration(locations = "/context.xml")
+@ContextConfiguration(locations = {"/context.xml" })
 public class IocTest {
 
     @Test
@@ -28,6 +31,29 @@ public class IocTest {
 
         Assertions.assertThat(ac.getBean("printer").toString() ).isEqualTo("Hello Spring");
     }
+
+    @Test
+    public void 계층구조테스트(){
+
+
+        ApplicationContext parent = new GenericXmlApplicationContext("/parentContext.xml");
+
+        GenericApplicationContext child = new GenericApplicationContext(parent);
+
+        XmlBeanDefinitionReader reader = new XmlBeanDefinitionReader(child);
+        reader.loadBeanDefinitions("/childContext.xml");
+        child.refresh();
+
+        Printer printer = child.getBean("printer", Printer.class);
+        Assertions.assertThat(printer).isNotNull();
+
+        Hello hello = child.getBean("hello", Hello.class);
+        Assertions.assertThat(hello).isNotNull();
+
+        hello.print();
+        Assertions.assertThat(printer.toString()).isEqualTo("Hello Child");
+    }
+
 
 
 }
